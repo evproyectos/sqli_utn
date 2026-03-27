387bf22bcdab12a94158d89377bda56ef9dc0753
Emanuel Vargas <evproyectosp@gmail.com>
2026-03-26 19:12:12 -0600

    V-02 Fixed V2

--- sqli_lab_vulnerable_app/app.py
+++ sqli_lab_vulnerable_app/app.py
@@ -230,11 +230,16 @@ def search():
         # ---------------------------------------------------
         # V-02: Búsqueda vulnerable también en una sola línea.
         # Payload: %' UNION SELECT id, username, password, role FROM users --
-        raw_query = f"SELECT id, title, author, category FROM books WHERE title LIKE '%{term}%' OR author LIKE '%{term}%' OR category LIKE '%{term}%'"
-
+        
+        
+        query = """
+        SELECT id, title, author, category
+        FROM books
+        WHERE title LIKE ? OR author LIKE ? OR category LIKE ?
+        """
         conn = get_connection()
         try:
-            books = conn.execute(raw_query).fetchall()
+            books = conn.execute(query, (f"%{term}%", f"%{term}%", f"%{term}%")).fetchall()
         except Exception as e:
             flash(f"Error en la base de datos: {e}", "error")
         conn.close()
