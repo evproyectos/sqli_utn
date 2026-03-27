dd20e154692b8e2af3aee48e1e964221fca7397b
Emanuel Vargas <evproyectosp@gmail.com>
2026-03-26 19:40:21 -0600

    V-05 Fixed

--- sqli_lab_vulnerable_app/app.py
+++ sqli_lab_vulnerable_app/app.py
@@ -292,4 +292,6 @@ def logout():
 # ---------------------------------------------------------------
 if __name__ == "__main__":
     init_db()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    debug_mode = os.environ.get("FLASK_DEBUG", "False") == "True"
+    app.run(host="0.0.0.0", port=5000, debug=debug_mode)
+
